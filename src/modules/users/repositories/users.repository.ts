dfc0e1fa144833e03f9cf34a8dd45fccf781b85b
martinsllm <lucaslm2010@gmail.com>
2024-07-24 20:48:57 -0300

    Implementing crud with Prisma

--- src/modules/users/repositories/users.repository.ts
+++ src/modules/users/repositories/users.repository.ts
@@ -1,39 +1,45 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
 import { UpdateUserDto } from '../dtos/update-user.dto';
-import { randomUUID } from 'node:crypto';
+import { PrismaService } from 'src/modules/global/prisma/prisma.service';
 
 @Injectable()
 export class UsersRepository {
-  private users: any[] = [];
+  constructor(private readonly prismaService: PrismaService) {}
 
-  create(user: CreateUserDto) {
+  async create(user: CreateUserDto) {
     const formatUser = {
       email: user.email,
       name: user.name,
-      id: randomUUID(),
     };
 
-    this.users.push(formatUser);
+    await this.prismaService.users.create({
+      data: formatUser,
+    });
 
     return formatUser;
   }
 
-  findAll() {
-    return this.users;
+  async findAll() {
+    return await this.prismaService.users.findMany();
   }
 
-  updateById(id: string, data: UpdateUserDto) {
-    const user = this.users.find((user) => user.id == id);
-
-    user.email = data.email;
-    user.name = data.name;
+  async updateById(id: string, data: UpdateUserDto) {
+    const user = await this.prismaService.users.update({
+      where: {
+        id,
+      },
+      data,
+    });
 
     return user;
   }
 
-  deleteById(id: string) {
-    const index = this.users.findIndex((user) => user.id == id);
-    this.users.splice(index, 1);
+  async deleteById(id: string) {
+    return await this.prismaService.users.delete({
+      where: {
+        id,
+      },
+    });
   }
 }
